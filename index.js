beae71dfc020da8d22e1fbefcb4a4b16551fe757
JoshGoold <joshgoold2@hotmail.com>
2025-02-06 11:36:30 -0500

    ...

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("dotenv").config()
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin: "http://localhost:3000"}))
 app.use(express.urlencoded({extended: true}))
 app.use("/", creater1)
 app.use("/", creater2)

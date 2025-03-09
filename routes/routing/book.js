db720c2d21169c3111ba548960def20dd1d18ed8
JoshGoold <155192266+JoshGoold@users.noreply.github.com>
2025-03-08 21:28:22 -0500

    Update book.js

--- routes/routing/book.js
+++ routes/routing/book.js
@@ -29,17 +29,17 @@ router.get("/book", async (req, res)=> {
         // If there are available or possible slots, send a success response
         console.log(available);
         console.log(possible);
-        const confirmedAvailable = await getGrokCheck(long, lat, available);
-        let fillSchedules = [];
-        if (confirmedAvailable.available.length > 0) {
-            const baseDate = confirmedAvailable.available[0].date.split("T")[0]; // "2025-03-12"
-            const startDate = new Date(baseDate);
-            startDate.setDate(startDate.getDate() + 2); // Move to next day, e.g., "2025-03-13"
-            fillSchedules = await getUpcomingSchedules();
+        //const confirmedAvailable = await getGrokCheck(long, lat, available);
+        //let fillSchedules = [];
+        //if (confirmedAvailable.available.length > 0) {
+            //const baseDate = confirmedAvailable.available[0].date.split("T")[0]; // "2025-03-12"
+            //const startDate = new Date(baseDate);
+            //startDate.setDate(startDate.getDate() + 2); // Move to next day, e.g., "2025-03-13"
+            //fillSchedules = await getUpcomingSchedules();
         }
-        //const fillSchedules = await getUpcomingSchedules();
+        const fillSchedules = await getUpcomingSchedules();
         // Combine confirmedAvailable and fillSchedules
-        const allAvailable = [...confirmedAvailable.available, ...fillSchedules];
+        const allAvailable = [...available, ...fillSchedules];
 
         return res.send({
             Message: "We've found some possible booking times",
